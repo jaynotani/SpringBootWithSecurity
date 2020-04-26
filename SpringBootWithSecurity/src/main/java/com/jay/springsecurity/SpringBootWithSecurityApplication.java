1c286822ebfb1c06c3e6e12f3c30933a242a6946
Jai <Jai@DESKTOP-HIGDOA7>
2020-04-26 20:35:04 +0530

    Added changes to run on external tomcat application

--- SpringBootWithSecurity/src/main/java/com/jay/springsecurity/SpringBootWithSecurityApplication.java
+++ SpringBootWithSecurity/src/main/java/com/jay/springsecurity/SpringBootWithSecurityApplication.java
@@ -2,10 +2,17 @@ package com.jay.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootWithSecurityApplication {
+public class SpringBootWithSecurityApplication extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootWithSecurityApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootWithSecurityApplication.class, args);
 	}
